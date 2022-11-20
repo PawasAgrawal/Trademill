325deed8a321563a12e9171b43fc6ba59d6df21b
pawas <pawas.ag2000@gmail.com>
2022-11-21 01:20:30 +0530

    default

---
+++ src/resources/stock.js
@@ -0,0 +1,57 @@
+import { iex } from '../config/iex.js';
+
+export const stock = {
+    
+    //if we want to change the data source, we can just change the url and api
+    latestPrice: (ticker, callback) => {
+        fetch(stock.latestPriceURL(ticker))
+        .then((response) => response.json())
+        .then((data) => callback(stock.formatPriceData(data)))
+    },
+    
+    latestPriceURL:  (ticker) =>{
+        return `${iex.base_url}/stock/${ticker}/intraday-prices?chartLast=1&token=${iex.api_token}`
+    },
+    
+    //if we change the data source, we also have to change the formatter accordingly
+    formatPriceData: (data) => {
+        console.log(data)
+        const stockData = data[data.length - 1]
+        const formattedData = {}
+        formattedData.price = stock.close
+        formattedData.date = stockData.date
+        formattedData.time = stockData.label
+        return formattedData
+    },
+
+    getYesterdaysClose: (ticker, lastTradingDate, callback) =>{
+        if(lastTradingDate != "" && lastTradingDate != undefined){  
+            console.log('here')
+            const url = stock.yesterdaysCloseURL(ticker, stock.formatDate(lastTradingDate))
+            fetch(url)
+            .then((response) => response.json())
+            .then((data) => callback(stock.formatPriceData(data)))
+        }
+    },
+
+    getLastTradingDate: () => {
+        const today = new Date().toISOString().split('T')[0].replace(/-/g, '');
+        const url = `${iex.base_url}/ref-data/us/dates/trade/last/1/${today}?token=${iex.api_token}`
+
+        return fetch(url).then((res) => res.json());
+    },
+
+    // exactDate=20221115&
+    yesterdaysCloseURL: (ticker, lastTradingDate) =>{
+        // var lastTradingDate = stock.formatDate(date);
+        console.log(lastTradingDate)
+        return `${iex.base_url}/stock/${ticker}/intraday-prices?chartLast=1&exactDate=${lastTradingDate}&token=${iex.api_token}`
+    },
+
+    formatDate: (date) => {
+        return new Date(date).toISOString().split('T')[0].replace(/-/g, '');
+    }
+}
+
+
+// https://cloud.iexapis.com/stable/stock/wmt/intraday-prices?chartLast=1&exactDate=20221117&token=pk_49620e53770040a3b71f5dcb11bd6abf
